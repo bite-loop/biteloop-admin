b9116c88bf32e73712373fcf900fa6a917a11400
rohan24may <rohanabcd0305@gmail.com>
2026-07-08 00:42:53 +0530

    feat: integrate AlertDialog component and add toast notifications for restaurant approval/rejection actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,9 +39,15 @@ export default function RootLayout({
   className={`dark ${geistSans.variable} ${geistMono.variable} ${jetbrainsMono.variable} h-full antialiased font-sans`}
 >
 <body className="min-h-full flex flex-col">
-  <ThemeProvider>
-    {children}
-  </ThemeProvider>
+<ThemeProvider>
+  {children}
+
+  <Toaster
+    position="top-right"
+    richColors
+    closeButton
+  />
+</ThemeProvider>
 </body>
     </html>
   );
